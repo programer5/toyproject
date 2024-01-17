bac680c38cb17ebb09f67e6b59c4dc3fba385edf
JungMinSeo <neverdie4757@gmail.com>
2024-01-17 23:41:11 +0900

    쿠키를 통한 인증 및 검증

--- src/main/java/com/toyproject/toyproject/api/config/AuthResolver.java
+++ src/main/java/com/toyproject/toyproject/api/config/AuthResolver.java
@@ -4,13 +4,17 @@ import com.toyproject.toyproject.api.config.data.UserSession;
 import com.toyproject.toyproject.api.domain.Session;
 import com.toyproject.toyproject.api.exception.Unauthorized;
 import com.toyproject.toyproject.api.repository.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -23,10 +27,21 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        String accessToken = webRequest.getHeader("Authorization");
-        if (accessToken == null || accessToken.equals("")) {
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+
+        if (servletRequest == null) {
+            log.error("servletRequest null");
             throw new Unauthorized();
         }
+
+        Cookie[] cookies = servletRequest.getCookies();
+
+        if (cookies.length == 0) {
+            log.error("쿠키가 없음");
+            throw new Unauthorized();
+        }
+
+        String accessToken = cookies[0].getValue();
 
         Session byAccessToken = sessionRepository.findByAccessToken(accessToken)
                 .orElseThrow(Unauthorized::new);
--- src/main/java/com/toyproject/toyproject/api/controller/AuthController.java
+++ src/main/java/com/toyproject/toyproject/api/controller/AuthController.java
@@ -1,14 +1,18 @@
 package com.toyproject.toyproject.api.controller;
 
 import com.toyproject.toyproject.api.request.Login;
-import com.toyproject.toyproject.api.response.SessionResponse;
 import com.toyproject.toyproject.api.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -18,9 +22,21 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody @Valid Login login) {
+    public ResponseEntity<Object> login(@RequestBody @Valid Login login) {
         String accessToken = authService.signin(login);
-        return new SessionResponse(accessToken);
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") // todo 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        log.info(">>>>>>> cookie={}", cookie.toString());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
     }
 
 }
