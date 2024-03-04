f19f80fe6e92de676af1ff00ab0ffc364a7bd6cd
JungMinSeo <neverdie4757@gmail.com>
2024-03-04 23:46:01 +0900

    시큐리티 폼 로그인 커스텀 설정

--- src/main/java/com/toyproject/toyproject/api/config/SecurityConfig.java
+++ src/main/java/com/toyproject/toyproject/api/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -21,8 +27,28 @@ public class SecurityConfig {
                         new AntPathRequestMatcher("/auth/login"),
                         new AntPathRequestMatcher("/h2-console/**")
                         ).permitAll().anyRequest().authenticated())
+                .formLogin(f -> f.usernameParameter("username").passwordParameter("password").loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")
+                        .defaultSuccessUrl("/"))
+                .userDetailsService(userDetailsService())
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("hodolman")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+        inMemoryUserDetailsManager.createUser(user);
+        return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
---
+++ src/main/java/com/toyproject/toyproject/api/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.toyproject.toyproject.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "메인 페이지입니다.";
+    }
+}
