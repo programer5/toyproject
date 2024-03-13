71b7215d5c3b93a3e6110d198d868e0deb083332
JungMinSeo <neverdie4757@gmail.com>
2024-03-13 22:17:53 +0900

    시큐리티 로그인 사용자 정보 가져오기

--- src/main/java/com/toyproject/toyproject/api/controller/MainController.java
+++ src/main/java/com/toyproject/toyproject/api/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.toyproject.toyproject.api.controller;
 
+import com.toyproject.toyproject.api.config.UserPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,7 @@ public class MainController {
     }
 
     @GetMapping("/user")
-    public String user() {
+    public String user(@AuthenticationPrincipal UserPrincipal userPrincipal) {
         return "사용자 페이지입니다.";
     }
 
